74ff4605ac103b0c5bf5f96afcebd366cec7e97c
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-03 16:16:02 -0300

    refactor: removed authentication routes and added user and admin routes

--- src/main/java/com/joaocarlos/securityjwt/api/controller/UserController.java
+++ src/main/java/com/joaocarlos/securityjwt/api/controller/UserController.java
@@ -1,13 +1,15 @@
 package com.joaocarlos.securityjwt.api.controller;
 
-import com.joaocarlos.securityjwt.api.DTO.LoginUserDTO;
-import com.joaocarlos.securityjwt.api.DTO.RecoveryJwtTokenDTO;
 import com.joaocarlos.securityjwt.api.DTO.RecoveryUserDTO;
-import com.joaocarlos.securityjwt.api.DTO.RegisterUserDTO;
 import com.joaocarlos.securityjwt.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -16,31 +18,25 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public RecoveryUserDTO createUser(@RequestBody RegisterUserDTO user){
-        return userService.signup(user);
-    }
-
-    @PostMapping("/login")
-    @ResponseStatus(HttpStatus.OK)
-    public RecoveryJwtTokenDTO authenticateUser(@RequestBody LoginUserDTO loginUserDTO){
-        return userService.authenticate(loginUserDTO);
-    }
-
     @GetMapping("/test")
     @ResponseStatus(HttpStatus.OK)
     public String getAuthenticationTest() {
         return "Autenticado com sucesso";
     }
 
-    @GetMapping("/test/user")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public String getUserAuthenticationTest() {
-        return "Cliente autenticado com sucesso";
+    public List<RecoveryUserDTO> allUsers(){
+        return userService.allUsers();
     }
 
-    @GetMapping("/test/admin")
+    @GetMapping("/user")
+    @ResponseStatus(HttpStatus.OK)
+    public RecoveryUserDTO getUserInfo() {
+        return userService.infoUser();
+    }
+
+    @GetMapping("/admin")
     @ResponseStatus(HttpStatus.OK)
     public String getAdminAuthenticationTest() {
         return "Administrador autenticado com sucesso";

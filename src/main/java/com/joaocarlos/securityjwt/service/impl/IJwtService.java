f601f567cc7d7c337c453e0087780d92f098438c
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-05 19:51:03 -0300

    style: remove unused imports

--- src/main/java/com/joaocarlos/securityjwt/service/impl/IJwtService.java
+++ src/main/java/com/joaocarlos/securityjwt/service/impl/IJwtService.java
@@ -2,12 +2,9 @@ package com.joaocarlos.securityjwt.service.impl;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTCreationException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.joaocarlos.securityjwt.domain.entity.User;
 import com.joaocarlos.securityjwt.service.JwtService;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
